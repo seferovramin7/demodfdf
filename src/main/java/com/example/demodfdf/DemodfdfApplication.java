119062bd143281cd579ad5e86875bb5ceb57cfeb
seferovramin7 <seferovramin7@gmail.com>
2022-07-30 21:52:41 +0400

    Initial commit

---
+++ src/main/java/com/example/demodfdf/DemodfdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodfdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodfdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodfdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodfdf/DemodfdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodfdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodfdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
